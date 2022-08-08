5fbdabdbd54f1c16e2d936e0caae69aeaa0ba8f8
Andrei Salauyou <Andrei_Salauyou@epam.com>
2022-08-08 17:51:02 +0300

    addUserTogroup method implemented

--- task/src/services/group/group.service.ts
+++ task/src/services/group/group.service.ts
@@ -1,12 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { GroupDalService } from 'src/data-access/dal/group.dal';
+import { UsersToGroupsDalService } from 'src/data-access/dal/groups-to-users.dal';
+import { UserDalService } from 'src/data-access/dal/user.dal';
 import { Group } from 'src/data-access/entities/group.entity';
 import { GroupDTO } from 'src/interfaces/dto/groupdto';
 
 @Injectable()
 export class GroupService {
-  constructor(private groupDal: GroupDalService) {}
+  constructor(
+    private groupDal: GroupDalService,
+    private usersGroupsDal: UsersToGroupsDalService,
+    private userDal: UserDalService,
+  ) {}
 
+  addUserToGroup(userId: string, groupId: string) {
+    this.userDal
+      .selectUserById(userId)
+      .then((user) => this.usersGroupsDal.addUserToGroup(user, groupId));
+  }
   findGroupById(userId: string): Promise<Group> {
     return this.groupDal.selectGroupById(userId);
   }
