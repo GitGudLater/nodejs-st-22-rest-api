8db20f366ebd8ac71cab1cbc4d17c13a6f6a28ac
Andrei Salauyou <Andrei_Salauyou@epam.com>
2022-08-15 18:09:29 +0300

    implemented simple filters for middleware exceptions

--- task/src/middleware/logger.middleware.ts
+++ task/src/middleware/logger.middleware.ts
@@ -5,8 +5,22 @@ import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   private readonly logger = new Logger(`HTTP`);
+
   use(req: Request, res: Response, next: NextFunction) {
     this.logger.debug(`requested path ${req.url} with method ${req.method}`);
+
+    process
+      .on('uncaughtException', (err, origin) => {
+        this.logger.error(
+          `Caught exception: ${err}\nException origin: ${origin}`,
+        );
+      })
+      .on('unhandledRejection', (reason, promise) => {
+        this.logger.error(
+          `Unhandled Rejection at: ${promise}, 'reason: ${reason}`,
+        );
+      });
+
     next();
   }
 }
