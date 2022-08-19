95ff9736ac0aba8f275c9f124a1fb1455ecedee4
Andrei Salauyou <Andrei_Salauyou@epam.com>
2022-08-19 11:45:11 +0300

    Fixed connection to db

--- task/src/modules/group/group.module.ts
+++ task/src/modules/group/group.module.ts
@@ -2,13 +2,20 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { GroupDalService } from 'src/data-access/dal/group.dal';
 import { UsersToGroupsDalService } from 'src/data-access/dal/groups-to-users.dal';
+import { UserDalService } from 'src/data-access/dal/user.dal';
 import { Group } from 'src/data-access/entities/group.entity';
+import { User } from 'src/data-access/entities/user.entity';
 import { GroupController } from 'src/routers/controllers/group/group.controller';
 import { GroupService } from 'src/services/group/group.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Group])],
+  imports: [TypeOrmModule.forFeature([Group, User])],
   controllers: [GroupController],
-  providers: [GroupService, GroupDalService, UsersToGroupsDalService],
+  providers: [
+    GroupService,
+    GroupDalService,
+    UsersToGroupsDalService,
+    UserDalService,
+  ],
 })
 export class GroupModule {}
