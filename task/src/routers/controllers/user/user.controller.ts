3c1ddb6c2af8edfcb6f8bbf30d622dbad1927341
Andrei Salauyou <Andrei_Salauyou@epam.com>
2022-08-26 14:49:32 +0300

    implemented auth for user component

--- task/src/routers/controllers/user/user.controller.ts
+++ task/src/routers/controllers/user/user.controller.ts
@@ -10,26 +10,31 @@ import {
   Put,
   Query,
   Res,
+  UseGuards,
 } from '@nestjs/common';
 import { UserDTO } from 'src/interfaces/dto/userdto';
 import { Response } from 'express';
 import { UserService } from 'src/services/user/user.service';
 import { User } from 'src/data-access/entities/user.entity';
+import { JwtAuthGuard } from 'src/guards/jwt/jwt-auth.guard';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Get()
   async getUsers(): Promise<User[]> {
     return await this.userService.getUsers();
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   async getUserById(@Param('id') id: string): Promise<User> {
     return await this.userService.findUserById(id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get()
   async getAutoSuggestUsers(
     @Query() query: { loginSubstring: string; limit: number },
@@ -50,12 +55,14 @@ export class UserController {
       : response.status(HttpStatus.CONFLICT).send('User login already exist');
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   @HttpCode(200)
   async markAsDelete(@Param('id') id: string) {
     this.userService.deleteUser(id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Put('id')
   updateUser(@Param('id') id: string, @Body() updatedUser: UserDTO) {
     return this.userService.updateUser(id, updatedUser);
